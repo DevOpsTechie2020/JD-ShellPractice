b5f0ceeb2d35a3f6e5ebe26f89c857538a2bc5cc
Pavan Bokka <rampavan.bokka@gmail.com>
2026-02-08 14:24:41 +0530

    code added

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -2,5 +2,5 @@
 
 echo "Suresh:: Hello Ramesh, How are you?"
 echo "Ramesh:: Hi suresh, I am fine thanks for asking. How are you doing?"
-echo "Suresh:: doing fine. What are you learning Ramesh?
+echo "Suresh:: doing fine. What are you learning Ramesh?"
 echo "Ramesh:: I am learning shell, What about you?"
