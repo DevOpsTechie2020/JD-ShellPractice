6e864afc22d5ab243e82965c4d7d02ea5066465d
Pavan Bokka <rampavan.bokka@gmail.com>
2026-02-08 14:44:09 +0530

    code added

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo "Please enter your username::"
-read USER_NAME
+read -s USER_NAME
 echo "Username is $USER_NAME"
+echo "please enter your password::"
+read -s PASSWORD
+echo "your password is $PASSWORD"
