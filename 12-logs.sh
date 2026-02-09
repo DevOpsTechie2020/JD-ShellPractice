b8fcc0fa2caf62c32c1101e45e71cfa21dff4ccf
Pavan Bokka <rampavan.bokka@gmail.com>
2026-02-09 15:46:01 +0530

    code added

---
+++ 12-logs.sh
@@ -0,0 +1,28 @@
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 is FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 is Success" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"
