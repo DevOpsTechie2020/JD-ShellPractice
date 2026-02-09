d41aae3d4a4adafe83976680ff1d4ec88001dcb0
Pavan Bokka <rampavan.bokka@gmail.com>
2026-02-09 10:27:51 +0530

    code added

---
+++ 10-Installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+echo "Installing Nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx...FAILURE"
+    exit1
+else
+    echo "Installing Nginx...SUCCESS"
+fi
