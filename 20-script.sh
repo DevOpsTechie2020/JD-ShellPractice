d1c59cf83cf65301df33d1d29ec0094136d88cb2
Pavan Bokka <rampavan.bokka@gmail.com>
2026-07-24 14:53:25 +0530

    code added

--- 20-script.sh
+++ 20-script.sh
@@ -1,7 +1,17 @@
-userid=$(id -u)
-if [ $userid -ne 0 ]; then
-    echo "please run this script with root user access"
-    exit 1
+#!/bin/bash
+# userid=$(id -u)
+# if [ $userid -ne 0 ]; then
+#     echo "please run this script with root user access"
+#     exit 1
+# else
+#     echo "You are running this script with root user access"
+# fi
+
+echo "installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Nginx installation is failure"
 else
-    echo "You are running this script with root user access"
+    echo "Nginx installation is success"
 fi
