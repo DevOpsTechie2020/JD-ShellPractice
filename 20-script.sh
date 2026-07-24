39c312c07e25d3d2ce90091300e367c61b7aa140
Pavan Bokka <rampavan.bokka@gmail.com>
2026-07-24 15:54:21 +0530

    code added

--- 20-script.sh
+++ 20-script.sh
@@ -7,32 +7,22 @@ else
     echo "You are running this script with root user access"
 fi
 
-echo "Installing nginx"
-dnf remove nginx -y
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 is Failure"
+        exit 1
+    else
+        echo "$2 is Success"
+}
 
-if [ $? -ne 0 ]; then
-    echo "Nginx uninstallation is failure"
-    exit 1
-else
-    echo "Nginx uninstallation is success"
-fi
+echo "Installing nginx"
+dnf install nginx -y
+VALIDATE $? "Installing nginx" 
 
 echo "Installing mqsql"
-dnf remove mysql -y
-
-if [ $? -ne 0 ]; then
-    echo "mysql uninstallation is failure"
-    exit 1
-else
-    echo "mysql uninstallation is success"
-fi
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
 
 echo "Installing nodejs"
-dnf remove nodejs -y
-
-if [ $? -ne 0 ]; then
-    echo "Nodejs uninstallation is failure"
-    exit 1
-else
-    echo "Nodejs uninstallation is success"
-fi
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
